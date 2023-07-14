de865c2352b2e029f9b9feb08c9310114a6a40e6
Daniel Ramos <mdrmtz@gmail.com>
2023-07-14 11:50:43 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+function loadCSS(url) {
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = url;
+  document.head.appendChild(link);
+}
+
+loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
