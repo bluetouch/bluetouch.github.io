b060b94393aedd06ce0861f83b6275d792ec4da6
Daniel Ramos <mdrmtz@gmail.com>
2023-07-14 12:11:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // script.js
-function loadCSS(url, media = 'all') {
+function loadCSS(url) {
   var link = document.createElement('link');
   link.rel = 'stylesheet';
   link.href = url;
-  link.media = media;
   document.head.appendChild(link);
 }
 
 loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
-loadCSS("styles.css", "media");
